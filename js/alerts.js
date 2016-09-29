aebe05484518c62f7d13d40c1792a069577bb931
Alex Dezon <alex@mindyourgap.com>
2016-09-29 10:45:15 +0200

    Libreria Cookies + ajustes Alerts e inclusion icono CLOSE en FONT

---
+++ js/alerts.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+    // OPEN
+    $('#header .icon-alerts').click(function () {
+        $('.alert-wrapper').css({
+            "opacity": 1,
+            "visibility": "visible"
+        });
+    });
+    // CLOSE
+    $('.alert-close').click(function () {
+        $('.alert-wrapper').css({
+            "opacity": 0,
+            "visibility": "hidden"
+        });
+    });
+
+    $(document).mouseup(function (e)
+    {
+        var container = $('.alert-wrapper .container');
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            $('.alert-wrapper').css({
+                "opacity": 0,
+                "visibility": "hidden"
+            });
+        }
+    });
+
+    // CLOSE ALERT SECTION MOBILE
+    $('#button-alerts-mobile .icon-close').click(function () {
+        $('#button-alerts-mobile').css({
+            "display": "none"
+        });
+    });
+
+
+
+
+});
