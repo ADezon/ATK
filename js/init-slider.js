7fcd9333900dbf74cf3ad3b12642035a8f5e940f
Alex Dezon <alex@mindyourgap.com>
2016-10-03 16:57:12 +0200

    Segundo SLIDER-Home (SMALLL, Only DESKTOP)

--- js/init-slider.js
+++ js/init-slider.js
@@ -1,46 +1,19 @@
 $(document).ready(function () {
 
-
     // FLEXSLIDER With PAGER
 
     // SEE BUG at:
     // https://github.com/woothemes/FlexSlider/issues/334
-    //store the slider in a local variable
+    // store the slider in a local variable
     var $window = $(window),
         flexslider = { vars:{} };
 
     // tiny helper function to add breakpoints
     function getGridSize() {
-        return ( (window.innerWidth || document.documentElement.clientWidth) < 600) ? 2 : 3;
+        return ( (window.innerWidth || document.documentElement.clientWidth) < 600) ? 2 : 4;
     }
 
     $window.load(function() {
-        // FLEXSLIDER
-        $('#home-slider').flexslider({
-            animation: "slide",
-            controlNav: false,
-            animationLoop: false,
-            //itemWidth: 100%
-            slideshow: false,
-            sync: "#home-carousel",
-        });
-
-        $('#home-carousel').flexslider({ // .padre-carousel
-            animation: "slide",
-            controlNav: false,
-            //directionNav:false,
-            animationLoop: false,
-            slideshow: false,
-            itemWidth: 210,
-            itemMargin: 10,
-            minItems: getGridSize(),
-            maxItems: getGridSize(),
-            asNavFor: '#home-slider',
-            start: function (slider) {
-                flexslider = slider; //Initializing flexslider here.
-            }
-        });
-    });
 
         // check grid size on resize event
         $window.resize(function () {
@@ -49,9 +22,28 @@ $(document).ready(function () {
             flexslider.vars.maxItems = gridSize;
         });
 
-    // SINGLE FLEXSLIDER.
-    $('.slider').flexslider({
-        animation: "slide"
+        // SINGLE FLEXSLIDER.
+        $('.slider').flexslider({
+            animation: "slide",
+            slideshowSpeed: timering
+        });
+
+        // SINGLE FLEXSLIDER.
+        $('.slider-four').flexslider({
+            animation: "slide",
+            slideshow: false,
+            itemWidth: 310,
+            itemMargin: 10,
+            minItems: getGridSize(),
+            maxItems: getGridSize(),
+            start: function (slider) {
+                flexslider = slider; //Initializing flexslider here.
+            }
+        });
+
+
     });
+
+
 
 });
