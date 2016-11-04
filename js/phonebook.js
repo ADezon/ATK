5e0300f8bf2e27c0c94b14092bebf6aa78a0c695
Alex Dezon <alex@mindyourgap.com>
2016-11-04 08:13:59 +0100

    Ajustes COPYs + Avance GUIA (html ready for integration)

--- js/phonebook.js
+++ js/phonebook.js
@@ -2,46 +2,6 @@ $(document).ready(function () {
 //
 // PHONEBOOK JS
 //
-//
-// MOVE ELEMENTS
-//(CONTENT VISIBLE AFTER CHANGES) (ADD LOADING GIF)
-    // SEARCH
-        // MOVE ADVANCED SEARCH SECTION AFTER LINK ADVANCED SEARCH
-        $(".linea_cerca .cerca_avancada_section").each(function(){
-            $(this).insertAfter($(this).next(".capsa_cerca"));
-        });
-
-        // MOVE ALL ADVANCED SEARCH AFTER INPUT SEARCH
-        $(".linea_cerca").each(function(){
-            $(this).insertAfter($(this).next(".rao_social"));
-        });
-
-        // MOVE CLEAN SEARCH IN ADVANCED SEARCH SECTION
-        $(".netejar_form").each(function(){
-            $(this).appendTo($(this).prev(".linea_cerca").children(".cerca_avancada_section"));
-        });
-
-    // RESULTS
-        // DELETE MAP LINK OPEN (MAP IS VISIBLE)
-        $(".companyDetail .companyInfo .companyMapClosed").each(function(){
-            $(this).remove();
-        });
-        // MOVE MAP (FIRST ELEMENT OF companyInfo)
-        $(".companyDetail .companyInfo .companyMap").each(function(){
-            $(this).prependTo($(this).parent());
-        });
-        // MOVE CONTACT (LAST ELEMENT OF companyInfo)
-        $(".companyDetail .companyInfo .companyContact").each(function(){
-            $(this).appendTo($(this).parent());
-        });
-        // MOVE ADDRESS AND SENDOPTIONS (ADD WRAPPER)
-        $(".companyInfo .companyAddress").each(function(index) {
-            $(this).next(".companySendOptions ").andSelf().wrapAll("<div class='companyAddressSendOptions'></div>")
-        });
-        // MOVE ADDRESS/SENDOPTIONS AND CONTACT(ADD WRAPPER)
-        $(".companyInfo .companyAddressSendOptions").each(function(index) {
-            $(this).next(".companyContact ").andSelf().wrapAll("<div class='right'></div>")
-        });
 
 //
 // END PHONEBOOK
--- registro_pending_email.php
+++ registro_pending_email.php
@@ -6,17 +6,17 @@
 
         <?php include 'includes/breadcrumb.php'; ?>
 
-        <h1 class="main-title">Benvinguda, Carla!</h1>
+        <h1 class="main-title">Estàs a un pas de registrar-te.</h1>
         <div class="request-successfully">
             <div class="icon-request "><span class="icons icon-check"></span></div>
-            <p class="text-request">La teva sol·licitud s'ha processat amb èxit.</p>
+            <p class="text-request">La teva sol·licitud està en procés.</p>
         </div>
         <div class="field-body">
-            <p>Ara ja pots gaudir de tots els avantatges i serveis que t’oferim a la nostra web.</p>
+            <p>A continuació, rebràs un correu on hauràs de confirmar la teva adreça de correu electrònic.</p>
         </div>
 
         <a class="button-pink bp-w-auto left" href="#">TORNAR A LA PÀGINA INICIAL</a>
-
+        
     </div>
 </div>
 <!--END SPECIFIC PAGE REGISTER CONFIRM-->
