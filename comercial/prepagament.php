544b37d47b0b4ccadb31b3d4271e58b44e0fab53
Alex Dezon <alex@mindyourgap.com>
2017-03-29 17:53:46 +0200

    Ajustes varios

--- comercial/prepagament.php
+++ comercial/prepagament.php
@@ -1,182 +1,183 @@
 <?php include 'includes/header.php'; ?>
 
-
+  <div id="main-content">
     <!--BEGIN PREPAGAMENT PAGE-->
     <div id="main-content" class="cont-wrapper with-bottom">
-        <div class="content">
+      <div class="content">
           <?php include 'includes/breadcrumb.php'; ?>
 
-            <h1 class="main-title">Prepagament</h1>
-            <p class="seventy">Amb la targeta de prepagament mòbil d'Andorra
-                Telecom (Mobiland
-                Clic!), oblida't de quotes, de factures i truca a qui vulguis,
-                tant des d'Andorra com des de l'estranger.
-                <br/>
-                Tu decideixes quina despesa vols fer.
-            </p>
-        </div>
-        <div class="content">
+        <h1 class="main-title">Prepagament</h1>
+        <p class="seventy">Amb la targeta de prepagament mòbil d'Andorra
+          Telecom (Mobiland
+          Clic!), oblida't de quotes, de factures i truca a qui vulguis,
+          tant des d'Andorra com des de l'estranger.
+          <br/>
+          Tu decideixes quina despesa vols fer.
+        </p>
+      </div>
+      <div class="content">
 
-        </div>
+      </div>
     </div>
 
     <div class="cont-wrapper bg-lightpink with-bottom">
-        <div class="content">
-            <div class="big-content-header">
-                <div class="big-icon-container"><span
-                            class="icons icon-sim"></span>
-                </div>
-                <div class="title-container">
-                    <div class="h-one-like">Recarregar saldo</div>
-                    <div class="soft-title">a la tarjeta Mobiland Clic!</div>
-                </div>
-            </div>
-
-            <div class="align-w-big-icon-container">
-                <p>Pots comprar les targetes de prepagament mòbil Mobiland Clic!
-                    a
-                    les <a href="#">oficines d'Andorra Telecom</a>, a la botiga
-                    <a href="#">Connectem</a>
-                    o a qualsevol dels nostres distribuïdors autoritzats.</p>
-
-                <p>Cada targeta Mobiland Clic! inclou un saldo inicial de 30
-                    euros
-                    i té un període de validesa de cinc mesos per esgotar el
-                    crèdit.
-                    Podràs fer trucades fins que s’acabi el saldo o fins que
-                    passin
-                    aquests cinc mesos sense fer-ne cap recàrrega; llavors, el
-                    saldo
-                    passarà a ser de 0 euros.</p>
-
-                <p>Un cop el saldo és de 0 euros, tens vuit mesos per recarregar
-                    la
-                    targeta. Durant aquest temps, només podràs rebre trucades
-                    quan
-                    estiguis sota cobertura Mobiland. Si, passats aquests vuit
-                    mesos, no has recarregat la targeta, perdràs el número de
-                    telèfon.
-                </p>
-            </div>
+      <div class="content">
+        <div class="big-content-header">
+          <div class="big-icon-container"><span
+                class="icons icon-sim"></span>
+          </div>
+          <div class="title-container">
+            <div class="h-one-like">Recarregar saldo</div>
+            <div class="soft-title">a la tarjeta Mobiland Clic!</div>
+          </div>
         </div>
+
+        <div class="align-w-big-icon-container">
+          <p>Pots comprar les targetes de prepagament mòbil Mobiland Clic!
+            a
+            les <a href="#">oficines d'Andorra Telecom</a>, a la botiga
+            <a href="#">Connectem</a>
+            o a qualsevol dels nostres distribuïdors autoritzats.</p>
+
+          <p>Cada targeta Mobiland Clic! inclou un saldo inicial de 30
+            euros
+            i té un període de validesa de cinc mesos per esgotar el
+            crèdit.
+            Podràs fer trucades fins que s’acabi el saldo o fins que
+            passin
+            aquests cinc mesos sense fer-ne cap recàrrega; llavors, el
+            saldo
+            passarà a ser de 0 euros.</p>
+
+          <p>Un cop el saldo és de 0 euros, tens vuit mesos per recarregar
+            la
+            targeta. Durant aquest temps, només podràs rebre trucades
+            quan
+            estiguis sota cobertura Mobiland. Si, passats aquests vuit
+            mesos, no has recarregat la targeta, perdràs el número de
+            telèfon.
+          </p>
+        </div>
+      </div>
     </div>
 
 
     <div class="cont-wrapper">
-        <div class="content prepagament">
+      <div class="content prepagament">
 
-            <div class="half-page">
-                <div class="big-content-header">
-                    <div class="big-icon-container"><span
-                                class="icons icon-call-money"></span>
-                    </div>
-                    <div class="title-container">
-                        <div class="h-one-like">Recarregar dades</div>
-                        <div class="soft-title">a la tarjeta Mobiland Clic!
-                        </div>
-                    </div>
-                </div>
-                <p>
-                    Abans de quedar-te a 0 euros de saldo, et recomanem que
-                    recarreguis la teva Clic!. D’aquesta manera, allargaràs la
-                    vida de la targeta, ja que cada recàrrega té un període de
-                    validesa que es va acumulant.
-                </p>
+        <div class="half-page">
+          <div class="big-content-header">
+            <div class="big-icon-container"><span
+                  class="icons icon-call-money"></span>
+            </div>
+            <div class="title-container">
+              <div class="h-one-like">Recarregar dades</div>
+              <div class="soft-title">a la tarjeta Mobiland Clic!
+              </div>
+            </div>
+          </div>
+          <p>
+            Abans de quedar-te a 0 euros de saldo, et recomanem que
+            recarreguis la teva Clic!. D’aquesta manera, allargaràs la
+            vida de la targeta, ja que cada recàrrega té un període de
+            validesa que es va acumulant.
+          </p>
 
-                <div class="box-info-data">
-                    <h4>Import</h4>
-                    <select>
-                        <option>15 € (2 mesos de validesa)</option>
-                        <option>18 € (3 mesos de validesa)</option>
-                        <option>30 € (12 mesos de validesa)</option>
-                    </select>
-                    <h4>Número mòbil</h4>
-                    <div class="modul-prepagament">
-                        <div>+376</div>
-                        <input type="text"> <input type="submit"
-                                                   class="button-pink"
-                                                   value="Recarregar">
-                    </div>
-
-                </div>
-
-
-                <p>
-                    <strong>També pots recarregar:</strong>
-                </p>
-                <ul class="line-bullet">
-                    <li>A caixers automàtics autoritzats (Crèdit Andorrà i
-                        Andbanc).
-                    </li>
-                    <li>Des del portal d’Andorra Telecom o de <a href="#">MoraBanc</a>,
-                        mitjançant
-                        el pagament amb targeta bancària.
-                    </li>
-                </ul>
-
-                <p>
-                    Consulta les condicions dels serveis mobils <a
-                            href="#">aquí</a>.
-                </p>
-
-
+          <div class="box-info-data">
+            <h4>Import</h4>
+            <select>
+              <option>15 € (2 mesos de validesa)</option>
+              <option>18 € (3 mesos de validesa)</option>
+              <option>30 € (12 mesos de validesa)</option>
+            </select>
+            <h4>Número mòbil</h4>
+            <div class="modul-prepagament">
+              <div>+376</div>
+              <input type="text" class="white_not_grey"> <input type="submit"
+                                         class="button-pink"
+                                         value="Recarregar">
             </div>
 
-            <div class="half-page">
-                <div class="big-content-header">
-                    <div class="big-icon-container"><span
-                                class="icons icon-web"></span>
-                    </div>
-                    <div class="title-container">
-                        <div class="h-one-like">Com obtenir</div>
-                        <div class="soft-title">la tarjeta Mobiland Clic!</div>
-                    </div>
-                </div>
-
-                <p><strong>Pots recarregar dades connectant-te a Internet a
-                        través
-                        de mòbil, tauleta o ordinador.</strong>
-                </p>
-                <p>Per saber el saldo de MB, envia un SMS gratuït al 123 (sense
-                    text
-                    o unicament un espai).</p>
+          </div>
 
 
-                <div class="box-info-data">
-                    <div class="full-centered"><span class="hard">Obté el forfet de dades enviant un SMS </span>(sense
-                        text)
-                    </div>
-                    <div class="gallery">
+          <p>
+            <strong>També pots recarregar:</strong>
+          </p>
+          <ul class="line-bullet">
+            <li>A caixers automàtics autoritzats (Crèdit Andorrà i
+              Andbanc).
+            </li>
+            <li>Des del portal d’Andorra Telecom o de <a href="#">MoraBanc</a>,
+              mitjançant
+              el pagament amb targeta bancària.
+            </li>
+          </ul>
 
-                        <div>
-                            <p class="pink">50 MB / 24h</p>
-                            <p class="hard">1 €</p>
-                            <p class="action-message">SMS AL 101</p>
-                        </div>
-
-                        <div>
-                            <p class="pink">50 MB / 24h</p>
-                            <p class="hard">1 €</p>
-                            <p class="action-message">SMS AL 101</p>
-                        </div>
-
-                        <div>
-                            <p class="pink">50 MB / 24h</p>
-                            <p class="hard">1 €</p>
-                            <p class="action-message">SMS AL 101</p>
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
+          <p>
+            Consulta les condicions dels serveis mobils <a
+                href="#">aquí</a>.
+          </p>
 
 
         </div>
+
+        <div class="half-page">
+          <div class="big-content-header">
+            <div class="big-icon-container"><span
+                  class="icons icon-web"></span>
+            </div>
+            <div class="title-container">
+              <div class="h-one-like">Com obtenir</div>
+              <div class="soft-title">la tarjeta Mobiland Clic!</div>
+            </div>
+          </div>
+
+          <p><strong>Pots recarregar dades connectant-te a Internet a
+              través
+              de mòbil, tauleta o ordinador.</strong>
+          </p>
+          <p>Per saber el saldo de MB, envia un SMS gratuït al 123 (sense
+            text
+            o unicament un espai).</p>
+
+
+          <div class="box-info-data">
+            <div class="full-centered"><span class="hard">Obté el forfet de dades enviant un SMS </span>(sense
+              text)
+            </div>
+            <div class="gallery">
+
+              <div>
+                <p class="pink">50 MB / 24h</p>
+                <p class="hard">1 €</p>
+                <p class="action-message">SMS AL 101</p>
+              </div>
+
+              <div>
+                <p class="pink">50 MB / 24h</p>
+                <p class="hard">1 €</p>
+                <p class="action-message">SMS AL 101</p>
+              </div>
+
+              <div>
+                <p class="pink">50 MB / 24h</p>
+                <p class="hard">1 €</p>
+                <p class="action-message">SMS AL 101</p>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
 
     </div>
 
     <!--END PREPAGAMENT PAGE-->
 
+  </div>
 
 <?php include 'includes/footer.php'; ?>
