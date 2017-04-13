481093f821ccb0aebaacf20d8fceae70929845e1
Alex Dezon <alex@mindyourgap.com>
2017-04-13 10:30:19 +0200

    Ajustes CSS products

--- comercial/client_products_mobil_contractats.php
+++ comercial/client_products_mobil_contractats.php
@@ -22,7 +22,7 @@
             <option>123456795</option>
           </select>
         </div>
-        <a href="#" class="button-pink">Cercar</a>
+        <input type="submit" class="button-pink" value="Cercar">
       </div>
     </div>
 
@@ -88,9 +88,7 @@
                   <!-- NOTE for ISOCO: each HTML structure IS DIFFERENT depending on STATUS !!! -->
                   <div class="products-block-forfeit odd">
                     <div class="forfeit-info">
-                      <div class="forfeit-name">
-                        Forfet
-                      </div>
+                      <div class="forfeit-name">Forfet</div>
                       <div class="big-info-large">Entrepreneur</div>
                       <h4>Forfet de dades <span class="bigger">3 GB</span></h4>
                       <a href="#" class="cancel-dades">Cancel·lar dades</a>
